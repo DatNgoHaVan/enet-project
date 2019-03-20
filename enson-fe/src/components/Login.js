5751a6783e8a7e631905b2a0dc5fcfb6693a3ef4
anthony <Anthony@BC12-Anthony.enclave.com>
2019-03-20 13:54:31 +0700

    bug in css

---
+++ enson-fe/src/components/Login.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import '../css/Login.css'
+
+class Login extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor:'#00cc99'}}>
+                <div className="container" >
+                    <div className ="row">
+                        <div className="col-md-3"></div>
+                        <div className="col-md-6 div">
+                            <div style={{position:'absolute', top:'20%'}}>
+                                <div className="form-control" style={{height:'400px', borderStyle: 'inset', width:'450px'}}>
+                                    <h3 className='title' style={{textAlign:'center', marginTop:'4vh', fontSize:'40px'}}>ENSON</h3>
+                                    <h4 style={{textAlign:'center'}}>Login</h4>
+                                    <form>
+                                        <div>
+                                            <div>
+                                                <p style={{marginLeft:'5%'}}>Username: </p>
+                                                <input type="text" className="form-control" 
+                                                style={{
+                                                    width:'90%',
+                                                    marginLeft:'5%',
+                                                    marginTop:'-10px',
+                                                    marginBottom:'15px'
+                                                }}>
+                                                </input>
+                                            </div>
+                                            <div>
+                                            <p style={{marginLeft:'5%'}}>Password: </p>
+                                            <input type="password" className="form-control" 
+                                            style={{
+                                                width:'90%',
+                                                marginLeft:'5%',
+                                                marginTop:'-10px'
+                                            }}>
+                                            </input>
+                                            </div>
+                                            <div style={{textAlign:'right', marginRight:'5%'}}>
+                                                <a href='#'>Forgot your password?</a>
+                                            </div>
+                                            <div className="form-inline" style={{marginTop : '7%'}}>
+                                                <a href='#' style={{marginLeft:'5%'}}>Create account</a>
+                                                <div style={{textAlign: 'right',position:'absolute',left : '75%'}}>
+                                                <Button variant="primary" style={{width :'80px'}}  >Login</Button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
